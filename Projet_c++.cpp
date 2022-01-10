ef2a9d6e802a07f75d247e3bb4f8718f8cfcb51d
ruth <ruthb.2618@gmail.com>
2022-01-10 16:18:44 +0100

    modif4

--- Projet_c++.cpp
+++ Projet_c++.cpp
@@ -1,6 +1,32 @@
 // Projet_c++
-  // Dans ce projet, nous allons implémenter différentes fonctions de la librairie "String". 
-// je suis pas meriem lol
-//hello wooooorld, cest meriem
+// Dans ce projet, nous allons implémenter différentes fonctions de la librairie "String". En effet, nous créeons une classe STRING qui contiendra les différentes fonctions 
+// souhaitées. 
+// Pour ce faire, nous avons divisé le travail en trois parties. Premièrement, l'élève A (CHARFI merieme) s'occupera d'implémenter les fonctions suivantes: copy constructor
+// c_str(), size() , clear() , operator=(char) , operator+(const string&, const char*) 
+// Deuxièmement, l'élève B ( BENAYOUN Ruth) codera les fonctions suivantes: constructor from a c-string, length() , max_size() , resize(size_t, char) ,operator=(const string&) 
+// operator+(const string&, char)
+// Finalement, l'élève C (RACHIDI Salma) programmera les fonctions suivantes: destructor, capacity() , empty() , reserve(size_t) , operator=(const char*) , operator+(const string&, const string&)
+
+
+//création de la classe STRING 
+class string{
+public: 
+  char a[100];
+
+
+}
+
+
+
+string :: string (){
+  char a[100]=  "hello world";
+}
+
+string :: string (const char* a){ // constructor from a c-string .
+	char a[100]="hello world";
+  
+}
+
+
 
 
