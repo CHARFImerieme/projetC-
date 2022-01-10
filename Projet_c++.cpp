a67b1effc6f7031bf78d6e983d98f5853d428bbe
ruth <ruthb.2618@gmail.com>
2022-01-10 15:03:08 +0100

    Modif 2

--- Projet_c++.cpp
+++ Projet_c++.cpp
@@ -1,5 +1,6 @@
-Projet_c++
+// Projet_c++
   // Dans ce projet, nous allons implémenter différentes fonctions de la librairie "String". 
 // je suis pas meriem lol
 //hello wooooorld
 
+// essaiiiii 
