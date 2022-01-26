e658f197c3324da4611c491ffa11b007ca62fe29
salma <salma.rachidi@aalto.fi>
2022-01-26 19:43:09 +0100

    Repartition du code en 3 fichiers

--- Projet_c++.cpp
+++ Projet_c++.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Projet_c++.h"
 using std::cout;
 using std::endl;
 
@@ -8,33 +9,22 @@ using std::endl;
 // souhaitées. 
 // Pour ce faire, nous avons divisé le travail en trois parties. Premièrement, l'élève A (CHARFI merieme) s'occupera d'implémenter les fonctions suivantes: copy constructor
 // c_str(), size() , clear() , operator=(char) , operator+(const string&, const char*) 
-// Deuxièmement, l'élève B ( BENAYOUN Ruth) codera les fonctions suivantes: constructor from a c-string, length() , max_size() , resize(size_t, char) ,operator=(const string&) 
+// Deuxièmement, l'élève B ( BENAYOUN Ruth) codera les fonctions suivantes: constructor from str c-string, length() , max_size() , resize(size_t, char) ,operator=(const string&) 
 // operator+(const string&, char)
 // Finalement, l'élève C (RACHIDI Salma) programmera les fonctions suivantes: destructor, capacity() , empty() , reserve(size_t) , operator=(const char*) , operator+(const string&, const string&)
 
 
-//création de la classe STRING 
-class STRING{ 
-public: 
-  char *a; // liste de char
-  int length;
-  STRING();
-  STRING(const STRING &obj);
-  char * get_a();
-  
-  //~STRING();
-  STRING (const char* b);
 
-};
 
 
 STRING :: STRING (){ //le constructeur
-  a = new char[100];
+  str = new char[100];
+  str = {};
   length = 0;
 }
 
-STRING :: STRING(const char* b){ // constructor from a c-string, b finit par 0
-	a = new char[100];
+STRING :: STRING(const char* b){ // constructor from str c-string, b finit par 0
+	str = new char[100];
   length = 0;
   int i = 0;
   while(b[i] != '\0'){
@@ -42,33 +32,31 @@ STRING :: STRING(const char* b){ // constructor from a c-string, b finit par 0
     i++;
   }
   for(int i=0; i<length;i++){
-    a[i]=b[i];
+    str[i]=b[i];
     }
   
-};
+}
 
 STRING :: STRING(const STRING &obj) { // c'est la copie du constructeur 
    
-  a = new char[100];
-  *a= *obj.a;
-};
-
-char * STRING::get_a(){
-  return a;
+  str = new char[100];
+  str = obj.str;
 }
-//STRING ::~STRING(void) { // c'est le destructeur 
-   //delete a[];
-//};
 
-int main() {
-  STRING s;
-  STRING x("wesh");
-  STRING t(x);
-  cout <<s.a<< endl;
-  cout <<t.a<<endl;
-  cout <<x.a<<endl;
-  return 0;
-
+char* STRING::get_str(){
+ // std::cout << "get_str()" << std::endl;
+ /*
+  if (length == 0){
+     return NULL;} // a revoir
+  else{
+    return str;}*/
+  return str;
 }
+
+STRING ::~STRING() { // c'est le destructeur 
+   delete str;
+}
+
+
 
 
