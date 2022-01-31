87dc7f12ad884cbb2540482551585562bbd5a850
CHARFImerieme <94897558+CHARFImerieme@users.noreply.github.com>
2022-01-31 12:25:53 +0100

    Update main_s.cpp
    modification mise en forme

--- main_s.cpp
+++ main_s.cpp
@@ -10,16 +10,34 @@ int main() {
   cout <<"s " << s.length << s.get_str() <<" fin s" <<endl; 
   String x = String("wesh");
   cout << "wesh ? " << x.get_str() <<endl;
-  String t =  String{x};
+  String t =  String(x);
+
+ // //------- TEST PARTIE 2: ÉTUDIANT B  -------
+
+
+
+
+ // //------- TEST PARTIE 2: ÉTUDIANT B  -------
+
+
+
+  // //------- TEST PARTIE 3: ÉTUDIANT C -------
+  // -- TEST MÉTHODE CAPACITY : 
   cout <<"t " << t.get_str() << " capacity of t: "<< t.capacity() <<endl;
+
+  // -- TEST MÉTHODE EMPTY : 
   cout << "Are the strings empty ? " << endl;
   cout << "s " << s.empty() << endl;
   cout << "t " << x.empty() << endl;
   cout << "x " << x.empty() << endl;
+
+  // -- TEST MÉTHODE RESERVE :
   t.reserve(10);
   cout <<"t " << t.get_str() << " capacity of t: "<< t.capacity() <<endl;
   t = "Hello world";
   cout << sizeof("Hello world") << endl;
   cout <<"t " << t.get_str() << " capacity of t: "<< t.capacity() <<endl;
   return 0;
+
+  
 }
