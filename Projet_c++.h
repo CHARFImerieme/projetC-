42265bc8979f44e4e00ef3134d33aac25e741d63
CHARFImerieme <94897558+CHARFImerieme@users.noreply.github.com>
2022-01-31 13:19:41 +0100

    modif mise en forme

--- Projet_c++.h
+++ Projet_c++.h
@@ -2,16 +2,32 @@
 //création de la classe String 
 class String{ 
 public: 
-  String();
-  String(const String &obj);
-  String (const char* b);
   char * get_str();
+
+  // //------- VARIABLES PARTIE 1: ÉTUDIANT A -------
+  String();
+  String (const char* b);
+  char* c_str();
+  int size();
+  String& operator=(char b);
+  void clear();
+
+
+  // //------- VARIABLES PARTIE 2: ÉTUDIANT B -------
+  String(const String &obj);
+
+
+
+
+
+
+
+   // //------- VARIABLES PARTIE 3: ÉTUDIANT C -------
   bool empty();
   int capacity();
   ~String();
   void reserve(size_t st);
   String& operator=(const char* b);
-
   const int default_capacity = 1;
   char *str; // liste de char
   int length;
